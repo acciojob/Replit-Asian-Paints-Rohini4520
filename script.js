e743933845f346ed8dd984b6978f8e4b4809cb67
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-04-09 16:48:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const changeBtn = document.getElementById('change_button');
+const ReseBtn = document.getElementById('Reset');
+
+function resetGridColors {
+
+	for(int i =1;i<=9;i++){
+		const gridItem = document.getElementById(i.toString());
+        gridItem.style.backgroundColor = 'transparent';
+      }
+    }
+
+    changeBtn.addEventListener('click', () => {
+      const blockId = document.getElementById('block_id').value;
+      const color = document.getElementById('colour_id').value;
+
+      resetGridColors();
+
+      const target = document.getElementById(blockId);
+      if (target) {
+        target.style.backgroundColor = color;
+      } else {
+        alert("Invalid block ID! Please enter a number between 1 and 9.");
+      }
+    });
+
+    resetBtn.addEventListener('click', resetGridColors);
+	
+}
